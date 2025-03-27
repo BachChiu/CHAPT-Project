0a058e3173f39bf12b43ebd6ee4b838ebb31404c
Bach Chiu <chiuvibach2@gmail.com>
2025-03-27 17:27:57 -0400

    Revert "Merge branch 'kyler-branch' into bach-branch"
    This reverts commit b0bb445c9280c83b6fff34c2fef35350c7a9d835, reversing
    changes made to e466e13385f63605091a7e8f842cc4c94bd1e5ec.

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('home/', views.HomeView.as_view()),
     path('role/', views.roleView.as_view(), name='roleView'), #this will be deleted later
 ]
-
